dd5523af96e0a2e7af3b46495da3cc319c992471
Gary Hanley <gary@websiterescuers.com.au>
2021-07-26 16:38:23 +0800

    Week 3 - testcolour and calculator.sh

---
+++ week3/calculator.sh
@@ -0,0 +1,41 @@
+ #!/bin/bash
+
+blue="\e[34m"
+green="\e[32m"
+red="\e[31m"
+purple="\e[35m"
+endcolour="\e[0m"
+
+
+echo "Enter an Equation: "
+echo -e "${blue}Addition eg. 3 + 2 "
+echo -e "${green}Subtraction eg. 3 - 2 " 
+echo -e "${red}Multiplication eg. 3 x 2 "
+echo -e "${purple}Division eg. 4 / 2 ${endcolour}"
+
+read num1 vop num2
+
+
+case $vop in
+    +)
+    # code if match
+        echo -e "${blue}Answer = $(expr $num1 + $num2)"
+    ;;
+    -)
+    # code if match
+        echo -e "${green}Answer = $(expr $num1 - $num2)"
+    ;;
+    x)
+    # code if match
+        echo -e "${red}Answer = $(expr $num1 \* $num2)"
+    ;;
+    /) # default case
+        echo -e "${purple}Answer = $(expr $num1 / $num2)"
+    # code if default case
+    ;;
+esac
+
+
+
+exit 0
+
---
+++ week3/testColour.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+blue="\e[34m"
+green="\e[32m"
+red="\e[31m"
+purple="\e[35m"
+endcolour="\e[0m"
+
+echo -e "${blue}This is Blue text${endcolour}"
+echo -e "${green}This is Green text${endcolour}"
+echo -e "${red}This is Red text${endcolour}"
+echo -e "${purple}This is Purple text${endcolour}"
