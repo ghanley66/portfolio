336fd6276cfb65a663ad08241a4c6a20b991f869
Gary Hanley <gary@websiterescuers.com.au>
2021-08-09 23:05:08 +0800

    Add Week 1 scripts to portfolio

--- week1/second.sh
+++ week1/second.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#Copyright 2021 Gary Hanley - Scripting Languages Week 1.4
+#Created - 15/07/2021
+#Short Script - Enter your name at the command line
+#Allows input from the command line
+
 echo "Hi there!"
 echo "It's good to see you $1!"
 exit 0
